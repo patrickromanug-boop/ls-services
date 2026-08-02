b30d0b01626684e74b469a3a567eb4784564cd36
patrickromanug-boop <patrickromanug@gmail.com>
2026-08-03 01:23:17 +0300

    Implement custom ImageLoader and notification channel

--- app/src/main/java/com/example/lsservices/MainApplication.kt
+++ app/src/main/java/com/example/lsservices/MainApplication.kt
@@ -1,1 +1,20 @@
 
+package com.example.lsservices
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import com.example.lsservices.util.NotificationHelper
+
+class MainApplication : Application(), ImageLoaderFactory {
+    override fun onCreate() {
+        super.onCreate()
+        NotificationHelper.createNotificationChannel(this)
+    }
+    override fun newImageLoader(): ImageLoader = ImageLoader.Builder(this)
+        .memoryCache { MemoryCache.Builder(this).maxSizePercent(0.20).build() }
+        .diskCache { DiskCache.Builder().directory(cacheDir.resolve("image_cache")).maxSizeBytes(25*1024*1024).build() }
+        .build()
+}
